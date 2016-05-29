1a34948c9a5ca26772a9b493077186b33acc92e1
victor <chen116@usc.edu>
2016-05-29 14:46:24 -0400

    a

--- rerun.sh
+++ rerun.sh
@@ -1,15 +1,15 @@
 
 
 date && echo "start uni light uni moderate" && \
-./run_bench_Array_granular.sh myapp uni-light 10 uni-moderate && \
+#./run_bench_Array_granular.sh myapp uni-light 10 uni-moderate && \
 date && echo "start uni medium uni moderate" && \
-./run_bench_Array_granular.sh myapp uni-medium 10 uni-moderate && \
+#./run_bench_Array_granular.sh myapp uni-medium 10 uni-moderate && \
 date && echo "start uni heavy uni moderate" && \
 #./run_bench_Array_granular.sh myapp uni-heavy 10 uni-moderate && \
 date && echo "start uni heavy uni moderate" && \
 #./run_bench_Array_granular.sh myapp bimo-medium 10 uni-moderate && \
 
-#./run_bench_Array_granular.sh myapp bimo-medium 10 uni-longRTXen && \
+./run_bench_Array_granular.sh myapp bimo-medium 10 uni-longRTXen && \
 #date && echo "start bimo-medium uni moderate" && \
 #./run_bench_Array_granular_bimo_moderate.sh myapp bimo-medium 10 uni-moderate && \
 date && echo "starting ratio" && \
